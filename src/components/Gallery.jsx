8bd4d0c5b6735edbd061558c8530a0aa4341162d
Rangel Xavier <rangel.webp@gmail.com>
2024-03-29 11:08:20 -0300

    Criado galeria da página de jogos
    Galeria da página interna de jogos criada.

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../styles/slider.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+export default function Gallery({ sliders }) {
+	return (
+		<>
+			<Swiper
+				spaceBetween={32}
+				slidesPerView={5}
+				loop={true}
+				autoplay={{
+					delay: 5000,
+					disableOnInteraction: false,
+				}}
+				pagination={{
+					clickable: true,
+				}}
+				modules={[Autoplay, Pagination, Navigation]}
+				navigation={true}
+				className="slider-galeria overflow-hidden">
+				{sliders.map((item, index) => {
+					return (
+						<SwiperSlide key={index}>
+							<img
+								className="w-[384px] h-[266px] object-cover rounded"
+								src={item}
+								alt={"Imagem " + index + ""}
+							/>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</>
+	);
+}
