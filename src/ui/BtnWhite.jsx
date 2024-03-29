1dd1c97442fee417270a1ae7cccfd78c9452c5d6
Rangel Xavier <rangel.webp@gmail.com>
2024-03-29 11:05:36 -0300

    Página interna dos jogos adicionada várias seções
    Página interna dos jogos adicionada várias seções

--- src/ui/BtnWhite.jsx
+++ src/ui/BtnWhite.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function BtnWhite({ href, titulo, icone }) {
+export default function BtnWhite({ href, titulo, children }) {
 	return (
 		<a
 			href={href}
 			className="bg-white hover:bg-zinc-950 transition-all duration-500
     py-3 px-6 rounded-full text-zinc-950 hover:text-white flex flex-start justify-between items-center min-w-64">
 			{titulo}
-			<img src={icone} alt={titulo} />
+			{children}
 		</a>
 	);
 }
--- src/ui/Tag.jsx
+++ src/ui/Tag.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function TagGreen({ children }) {
+export default function Tag({ children }) {
 	return (
-		<span className="bg-zinc-800 text-zinc-400 text-[12px] font-semibold py-1 px-2 rounded">
+		<span className="bg-zinc-800 text-zinc-400 text-sm font-semibold py-1 px-2 rounded">
 			{children}
 		</span>
 	);
