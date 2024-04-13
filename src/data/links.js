7b5200176a0af9f95d4fff585d64fa0a252e4461
Rangel Xavier <rangel.webp@gmail.com>
2024-04-13 09:17:24 -0300

    Links corrigidos

---
+++ src/data/links.js
@@ -0,0 +1,17 @@
+export const links = {
+	facebook: "https://www.facebook.com/frozenbyte",
+	twich: "https://www.twitch.tv/frozenbyte",
+	youtube: "https://www.youtube.com/user/frozenbytegames",
+	frozenbyte: "https://www.frozenbyte.com",
+	steam: "https://store.steampowered.com/developer/frozenbyte",
+	instagram: "https://www.instagram.com/frozenbytegames/",
+	twitter: "https://twitter.com/frozenbyte",
+	press: "https://press.frozenbyte.com/",
+};
+
+export const navItemLinks = [
+	{ title: "Jogos", href: "#", dropdown: false },
+	{ title: "Complementos", href: "#", dropdown: true },
+	{ title: "Frozenbyte", href: "#", dropdown: false },
+	{ title: "Press", href: "#", dropdown: false },
+];
